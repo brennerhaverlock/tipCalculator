ad65924733bbcb8db686c271f6b99ee230893e09
brennerhaverlock <brenner.haverlock@gmail.com>
2020-09-03 14:59:39 -0400

    updated formatting
    VS code formatting

--- tipCalculator.py
+++ tipCalculator.py
@@ -1,22 +1,21 @@
 # calculate tip
 def calculateTax(cost, percentageTax):
-return (cost * (percentageTax / 100))
+    return (cost * (percentageTax / 100))
 
 
 def calculateCost(cost, percentageTip):
-return (cost * (percentageTip / 100))
-# Ask user for the cost
+    return (cost * (percentageTip / 100))
 
+
+# Ask user for the cost
 cost = float(input("Enter cost of bill(Exclude $ sign) \n"))
 
 # Ask user for tax percentage
+
 percentageTax = int(input("Enter a tax percentage (Exclude % sign)\n "))
 
-
 # Ask user for tip percentage
-
 percentageTip = int(input("Enter tip percentage as whole number  \n"))
-
 
 # Return cost multiplied by tax percentage
 costWithTax = calculateTax(cost, percentageTax)
