62c5c7215289810269a52ac43bee9736a3b67ea1
brennerhaverlock <brenner.haverlock@gmail.com>
2020-09-03 14:44:05 -0400

    added functions

--- tipCalculator.py
+++ tipCalculator.py
@@ -1,7 +1,10 @@
 # calculate tip
+def calculateTax(cost, percentageTax):
+return (cost * (percentageTax / 100))
 
 
-
+def calculateCost(cost, percentageTip):
+return (cost * (percentageTip / 100))
 # Ask user for the cost
 
 # Ask user for tax percentage
